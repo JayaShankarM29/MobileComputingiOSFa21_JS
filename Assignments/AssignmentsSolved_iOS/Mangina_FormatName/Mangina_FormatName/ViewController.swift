4086b4537dc41384d13b6d3ad81e1ffb569a1241
JayaShankar Mangina <79549340+JayaShankarM29@users.noreply.github.com>
2021-09-05 12:13:29 -0500

    Update ViewController.swift

--- Assignments/AssignmentsSolved_iOS/Mangina_FormatName/Mangina_FormatName/ViewController.swift
+++ Assignments/AssignmentsSolved_iOS/Mangina_FormatName/Mangina_FormatName/ViewController.swift
@@ -9,21 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+//Created an Interface Builder Outlet for firstname TextField and connected it to Interface Builder in Main.storyboard
     @IBOutlet weak var firstNameTextField: UITextField!
+    
+//Created an Interface Builder Outlet for lastName TextField and connected it to Interface Builder in Main.storyboard
     @IBOutlet weak var lastNameTextField: UITextField!
+    
+//Created an Interface Builder Outlet for Display Label and connected it to Interface Builder in Main.storyboard
     @IBOutlet weak var displayLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+
+//Created an Interface Builder Action for Submit Button and connected it to Interface Builder in Main.storyboard
     @IBAction func onClickOfSubmit(_ sender: UIButton) {
-        var fNameField = firstNameTextField.text!
-        var lNameField = lastNameTextField.text!
+        let fNameField = firstNameTextField.text!
+        let lNameField = lastNameTextField.text!
         displayLabel.text = "\(lNameField),\(fNameField)"
-        
-        
     }
+    
+//Created an Interface Builder Action for Reset Button and connected it to Interface Builder in Main.storyboard
     @IBAction func onClickOfReset(_ sender: UIButton) {
         firstNameTextField.text = ""
         lastNameTextField.text = ""
@@ -31,7 +38,5 @@ class ViewController: UIViewController {
         firstNameTextField.becomeFirstResponder()
     }
     
-    
-
 }
 
